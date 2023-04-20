1988cdf47e17d921f8a71980f1619ee580fbedfd
Russell Molimock <russellmolimock@gmail.com>
2023-04-20 13:21:14 -0500

    frontend styling using materialize

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'random_jokes#index'
   get 'random', to: 'random_jokes#index'
+  get '/new_dad_joke', to: 'random_jokes#new_dad_joke', as: 'new_dad_joke'
 
-  resources :jokes, only: [:create, :index]
-  delete 'jokes', to: 'jokes#destroy_all'
+  
+  resources :jokes, only: [:create, :index, :destroy]
+  delete '/jokes', to: 'jokes#destroy_all', as: :destroy_all_jokes
+  
 
 
 
